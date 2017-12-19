1b0e1b74e6b0b6e1c5c3ccbae01010dc66db4dec
AlainCVT <cvt.alain@gmail.com>
2017-12-19 11:51:24 +0100

    Update

--- src/js/components/slides.js
+++ src/js/components/slides.js
@@ -1,9 +1,5 @@
-const $slides = Array.from(document.querySelectorAll('.slide'))
-
-for (let index = 0 ; index < $slides.length ; index++)
-{
-    $slides[index].style.left = `${index * 100}%`
-}
+const $slider = document.querySelector('.slider')
+const $slides = Array.from($slider.querySelectorAll('.slide'))
 
 let isScrolling
 let deltaScroll = 0
@@ -20,60 +16,67 @@ window.addEventListener('mousewheel', (event) =>
     {
         // Get active slide
         const $activeSlide = $slides.find(slide => slide.classList.contains('active'))
+        console.log(deltaScroll)
 
         // Scroll down
-        if (deltaScroll > window.innerHeight / 10)
+        if (deltaScroll > 0)
         {
             // Below floor
             if (floor < Array.from($activeSlide.querySelectorAll('.bloc')).length - 1)
             {
                 floor++
-                $activeSlide.style.transform = `translateY(-${floor * 100}vh)`
-            }
-
-            // Right slide
-            else if (floor == Array.from($activeSlide.querySelectorAll('.bloc')).length - 1)
-            {
-                const index = $slides.indexOf($activeSlide)
-
-                // Not last slide
-                if (index < $slides.length - 1)
-                {
-                    floor = 0
-                    $activeSlide.classList.remove('active')
-                    $slides[index + 1].classList.add('active')
-                    document.body.style.transform = `translateX(-${(index + 1) * 100}vw)`
-                }
+                $activeSlide.style.transform = `translateY(-${Math.min(deltaScroll, window.innerHeight)}px)`
             }
         }
 
         // Scoll up
-        else if (deltaScroll < window.innerHeight / 10)
+        else if (deltaScroll < 0)
         {
-            // Uppon floor
-            if (floor > 0)
-            {
-                floor--
-                $activeSlide.style.transform = `translateY(-${floor * 100}vh)`
-            }
-
-            // Left slide
-            else if (floor == 0)
-            {
-                const index = $slides.indexOf($activeSlide)
-
-                // Not first slide
-                if (index > 0)
-                {
-                    floor = Array.from($slides[index - 1].querySelectorAll('.bloc')).length - 1
-                    $activeSlide.classList.remove('active')
-                    $slides[index - 1].classList.add('active')
-                    document.body.style.transform = `translateX(-${(index - 1) * 100}vw)`
-                }
-            }
+            console.log('up')
         }
 
         // Reinit variation
         deltaScroll = 0
     }, 75)
 })
+
+
+
+
+            // // Right slide
+            // else if (floor == Array.from($activeSlide.querySelectorAll('.bloc')).length - 1)
+            // {
+            //     const index = $slides.indexOf($activeSlide)
+
+            //     // Not last slide
+            //     if (index < $slides.length - 1)
+            //     {
+            //         floor = 0
+            //         $activeSlide.classList.remove('active')
+            //         $slides[index + 1].classList.add('active')
+            //         document.body.style.transform = `translateX(-${(index + 1) * 100}vw)`
+            //     }
+            // }
+
+
+            // // Uppon floor
+            // if (floor > 0)
+            // {
+            //     floor--
+            //     $activeSlide.style.transform = `translateY(-${floor * 100}vh)`
+            // }
+
+            // // Left slide
+            // else if (floor == 0)
+            // {
+            //     const index = $slides.indexOf($activeSlide)
+
+            //     // Not first slide
+            //     if (index > 0)
+            //     {
+            //         floor = Array.from($slides[index - 1].querySelectorAll('.bloc')).length - 1
+            //         $activeSlide.classList.remove('active')
+            //         $slides[index - 1].classList.add('active')
+            //         document.body.style.transform = `translateX(-${(index - 1) * 100}vw)`
+            //     }
+            // }
